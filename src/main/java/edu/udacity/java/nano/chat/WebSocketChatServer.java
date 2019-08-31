78bc470f67d5a02a025ed7e209ddb40c8f2fcf62
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-08-31 22:52:35 +0100

    Implemented server methods.

--- src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
+++ src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
@@ -1,5 +1,6 @@
 package edu.udacity.java.nano.chat;
 
+import com.alibaba.fastjson.JSON;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
@@ -22,7 +23,7 @@ public class WebSocketChatServer {
     private static Map<String, Session> onlineSessions = new ConcurrentHashMap<>();
 
     private static void sendMessageToAll(String message) {
-        onlineSessions.forEach((id, session) -> {
+        onlineSessions.forEach((id, session) ->{
             try {
                 session.getBasicRemote().sendText(message);
             } catch (IOException e) {
@@ -31,33 +32,24 @@ public class WebSocketChatServer {
         });
     }
 
-    /**
-     * Open connection, 1) add session, 2) add user.
-     */
     @OnOpen
     public void onOpen(Session session) {
-        //TODO: add on open connection.
+        onlineSessions.put(session.getId(), session);
+        sendMessageToAll(Message.jsonConverter("ENTER", "", "", onlineSessions.size()));
     }
 
-    /**
-     * Send message, 1) get username and session, 2) send message to all.
-     */
     @OnMessage
-    public void onMessage(Session session, String jsonStr) {
-        //TODO: add send message.
+    public void onMessage(Session session, String jsonConverter) {
+        Message message = (Message) JSON.parseObject(jsonConverter, Message.class);
+        sendMessageToAll(Message.jsonConverter("SPEAK", message.getUsername(), message.getMessage(), onlineSessions.size()));
     }
 
-    /**
-     * Close connection, 1) remove session, 2) update user.
-     */
     @OnClose
     public void onClose(Session session) {
-        //TODO: add close connection.
+        onlineSessions.remove(session.getId());
+        sendMessageToAll(Message.jsonConverter("QUIT", "", "", onlineSessions.size()));
     }
 
-    /**
-     * Print exception.
-     */
     @OnError
     public void onError(Session session, Throwable error) {
         error.printStackTrace();
