32b5e010da107575ef1d26fd2ca640f7f7b3b5c7
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-08-31 19:44:30 +0100

    Implemented sendMessageToAll

--- src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
+++ src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -18,13 +19,16 @@ import java.util.concurrent.ConcurrentHashMap;
 @ServerEndpoint("/chat")
 public class WebSocketChatServer {
 
-    /**
-     * All chat sessions.
-     */
     private static Map<String, Session> onlineSessions = new ConcurrentHashMap<>();
 
-    private static void sendMessageToAll(String msg) {
-        //TODO: add send message method.
+    private static void sendMessageToAll(String message) {
+        onlineSessions.forEach((id, session) -> {
+            try {
+                session.getBasicRemote().sendText(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     /**
