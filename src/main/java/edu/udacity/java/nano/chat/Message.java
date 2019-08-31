87d6d527ff9f0e0518909969e60a02f49d1c22d3
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-08-31 18:26:34 +0100

    Created Message constructor

--- src/main/java/edu/udacity/java/nano/chat/Message.java
+++ src/main/java/edu/udacity/java/nano/chat/Message.java
@@ -1,10 +1,66 @@
 package edu.udacity.java.nano.chat;
 
-import com.sun.xml.internal.bind.v2.TODO;
-
 /**
  * WebSocket message model
  */
 public class Message {
-	//TODO add message model
+
+	public static final String ENTER = "ENTER";
+	public static final String CHAT = "CHAT";
+	public static final String LEAVE = "LEAVE";
+	private String type;
+	private String username;
+	private String message;
+	private int onlineCount;
+
+	public Message(String type, String username, String message, int onlineCount) {
+
+		this.type = type;
+		this.username = username;
+		this.message = message;
+		this.onlineCount = onlineCount;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setOnlineCount(int onlineCount) {
+		this.onlineCount = onlineCount;
+	}
+
+	public int getOnlineCount() {
+		return onlineCount;
+	}
+	public String getENTER(){
+		return "ENTER";
+	}
+
+	public String getChat(){
+		return "CHAT";
+	}
+
+	public String getLeave(){
+		return "LEAVE";
+	}
 }
