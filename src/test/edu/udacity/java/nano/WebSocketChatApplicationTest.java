3a01b2b61ade3c05fabe628039b010a87e7b02ea
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-09-03 15:38:40 +0100

    Added test WebSocketChatApplication

---
+++ src/test/edu/udacity/java/nano/WebSocketChatApplicationTest.java
@@ -0,0 +1,27 @@
+package edu.udacity.java.nano;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebMvcTest(WebSocketChatApplication.class)
+public class WebSocketChatApplicationTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void shouldDisplayLoginScreen() throws Exception {
+		this.mockMvc.perform(get("/")).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Chat Room")));
+	}
+}
