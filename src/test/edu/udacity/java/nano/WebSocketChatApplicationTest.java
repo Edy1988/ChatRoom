bbabe70e7de32a427325ec9eb0556cf588e386c4
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-09-03 15:57:05 +0100

    Added testmethod to test /index

--- src/test/edu/udacity/java/nano/WebSocketChatApplicationTest.java
+++ src/test/edu/udacity/java/nano/WebSocketChatApplicationTest.java
@@ -14,6 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest(WebSocketChatApplication.class)
+
 public class WebSocketChatApplicationTest {
 
 	@Autowired
@@ -23,5 +24,11 @@ public class WebSocketChatApplicationTest {
 	public void shouldDisplayLoginScreen() throws Exception {
 		this.mockMvc.perform(get("/")).andExpect(status().isOk())
 				.andExpect(content().string(containsString("Chat Room")));
+	}
+
+	@Test
+	public void shouldDisplayChatPage() throws Exception {
+		this.mockMvc.perform(get("/index?")).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Welcome")));
 	}
 }
